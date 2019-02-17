192c94bed124283b20d78e11706cd45657e92cd5
Guy Mininberg <guy.mininberg@vonage.com>
2019-02-17 12:45:30 +0200

    ...

--- exercise_1.py
+++ exercise_1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import dpkt
+from scapy.all import *
 from twisted.protocols import sip #should be good sip lib but can use any other sip lib.
 import sys, getopt
 
@@ -11,10 +11,15 @@ def post_output_for_test():
 #code info that got the codec and and its payloadType
 class codecInfo(object):
     """docstring for dialog"""
-    def __init__(self, codecName, payloadType):
+    def __init__(self, codecName, payloadType, portSource, portDest):
         super(dialog, self).__init__()
         self.codecName = codecName
         self.payloadType = payloadType
+        self.portSource = portSource
+        self.portDest = portDest
+
+
+neg_codec_info = codecInfo("", -1)
 
 #sdp description:
 #codeinfo - array of all codecs.
@@ -37,13 +42,18 @@ def is_invite(buf):
     #check if the SIP is inivte or 200OK for invite
     pass
 
+def is_bye(buf):
+    #check if the call is ended.
+    pass
+
 def is_invite_dialog_close():
     #check if invite got 200 OK (can be both remote or local invite)
     pass
 
-def set_neg_codec(dialog):
+def set_neg_codec():
     #once a dialog is closed then get the codec we are using set the selected codecInfo
-    pass
+    neg_codec_info.codecName = "____the_name_of_the_codec_from_the_sdp____"
+    neg_codec_info.payloadType = -1 #the payload of the codec from the sdp.
 
 def read_sdp(buf):
     #read the sdp (invite and 200OK) use sdpDescription for saving data
@@ -62,21 +72,25 @@ def write_rtp_stream_to_file(stream, fileName):
     pass
 
 def is_local_rtp(buf):
-    #check if local rtp (by the ip)
-    pass
+    if neg_codec_info.portSource == sorce_port and neg_codec_info.portDest == dest_port:
+        return true
+    return false
 
 def check_is_sip(buf):
     pass
 
-def check_is_rtp(buf):
-    pass
+def check_is_rtp(sorce_port, dest_port):
+    if neg_codec_info.portSource == sorce_port and neg_codec_info.portDest == dest_port or neg_codec_info.portSource == dest_port and neg_codec_info.portDest == sorce_port:
+        return true
+    return false
 
 def check_is_rtcp(buf):
     pass
 
 def analyze_voip(pcap):
+    
     for line in pcap:
-        #take line an d decode it buffer
+        #take line and decode it to buffer
         buf = ''
         if check_is_sip(buf):
             #read and understand the sip message
@@ -86,15 +100,19 @@ def analyze_voip(pcap):
             if is_invite(buf):
                 read_sdp(buf)
                 if is_invite_dialog_close():
-                    set_neg_codec(dialog)
+                    set_neg_codec()
 
-        elif check_is_rtp(buf):
+            if is_bye(buf):
+                #the call is ended
+                break
+
+        elif check_is_rtp(_sorce_port_, _dest_port_):
             #read and understand the rtp message, and wtite it to file
             if check_rtp_valid(buf):
                 if is_local_rtp(buf):
-                    write_rtp_stream_to_file(convert_rtp_buf_to_stream(buf), localFileName)
+                    write_rtp_stream_to_file(convert_rtp_buf_to_stream(buf), localFileName_place_holder)
                 else:
-                    write_rtp_stream_to_file(convert_rtp_buf_to_stream(buf), remoteFileName)
+                    write_rtp_stream_to_file(convert_rtp_buf_to_stream(buf), remoteFileName_place_holder)
         elif check_is_rtcp(buf):
             #do nothing for now (will use rtcp in exercise 2)
             pass
@@ -107,7 +125,7 @@ def analyze_voip(pcap):
 def main(argv):
    inputfile = ''
    try:
-      opts, args = getopt.getopt(argv,"hi:")
+      opts, args = getopt.getopt(argv,"hi:o:")
    except getopt.GetoptError:
       print 'exercise_1.py -i <inputfile>'
       sys.exit()
@@ -123,12 +141,10 @@ def main(argv):
             print 'exercise_1.py -i <inputfile>'
             sys.exit()
 
-   print 'Input file is \n', inputfile
-   print 'Output path is \n', outputPath
-
-   with open(inputfile, 'rb') as f:
-        pcap = dpkt.pcap.Reader(f)
-        analyze_voip(pcap)
+    print 'Input file is \n', inputfile
+    print 'Output path is \n', outputPath
+    pcap = rdpcap(inputfile)
+    analyze_voip(pcap)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 echo "Welcome to Odisa 1st exercise"
 echo "installing helper pyhton libs"
 
-echo "install dpkt for pcap https://github.com/kbandla/dpkt"
-pip install dpkt
+echo "install scapy for pcap https://pypi.org/project/scapy"
+pip install scapy
 
 echo "install Twisted for sip https://github.com/racker/python-twisted-core"
 pip install Twisted
